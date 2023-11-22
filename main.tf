14709f11420cf7439b136e7b6d73c3f517acd8d2
sllim637 <slim.hammami1977@gmail.com>
2023-11-22 10:57:26 +0100

    adding security gorup to terrafom

--- main.tf
+++ main.tf
@@ -2,17 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Mon groupe de sécurité"
+
+  // Règles entrantes
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Règles sortantes
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "fromJenkinsInstance" {
-  ami           = "ami-0fc5d935ebf8bc3bc"  # Remplacez par l'AMI approprié
-  instance_type = "t2.micro"
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
 
   tags = {
     Name = "MonInstanceEC2FromJenkins"
-  }
-  lifecycle {
-    ignore_changes = [
-      instance_state
-    ]
   }
 }
 
