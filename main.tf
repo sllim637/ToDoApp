d13fdcf588955e85b10ffe027f71ca7b553a2be4
sllim637 <slim.hammami1977@gmail.com>
2023-11-23 11:37:05 +0100

    adding key_name tag

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_instance" "fromJenkinsInstance" {
   ami                    = "ami-0fc5d935ebf8bc3bc"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  key_name      = "shunter_ubuntu_key"  # Le nom de la clé telle qu'enregistrée dans AWS EC2
 
   tags = {
     Name = "MonInstanceEC2FromJenkins"
